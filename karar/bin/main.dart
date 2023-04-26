7475b10b031f7dc8d1b6e8e4ba44aa8816515bf9
Melih <119483611+melihkabael@users.noreply.github.com>
2023-04-26 13:08:52 +0300

    Add files via upload

---
+++ karar/bin/main.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+void main(List<String> args) {
+  Random rnd = Random();
+  int sayi1, sayi2;
+  
+  String islem = "+"
+  sayi1 = rnd.nextInt(100);
+  sayi2 = rnd.nextInt(100);
+
+  switch(islem){
+    case "+";
+    print("$sayi1+$sayi2=$(sayi1 + sayi2);
+      }
+
+}
